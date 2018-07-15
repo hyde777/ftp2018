6a8764fc634b0226645e33367a23c9b4fe141119
loghan <loghan.r@gmail.com>
2018-07-15 06:43:35 +0200

    Connexion identifiant mot de passe

--- client.c
+++ client.c
@@ -12,116 +12,122 @@
 #include	<fcntl.h>
 
 #define	max(a,b)	((a) > (b) ? (a) : (b))
-#define MAXLINE	500
+#define MAXCHAR	256
 
 void str_cli(int, int);
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	sockfd;
-	struct sockaddr_in servaddr;
+	int	clientSocket;
+	struct sockaddr_in serverAdresse;
 	int fd;
 
-	if (argc != 4)
+	if (argc != 3)
 	{
-		printf("usage: client <IPaddress> <Port> <source_données>\n");
+		printf("usage: client <IPaddress> <Port>\n");
 		exit(-1);
 	}
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	bzero(&serverAdresse, sizeof(serverAdresse));
 
-	bzero(&servaddr, sizeof(servaddr));
+	serverAdresse.sin_family = AF_INET;
+	serverAdresse.sin_port = htons(atoi(argv[2]));
+	inet_pton(AF_INET, argv[1], &serverAdresse.sin_addr);
+	
+	connect(clientSocket, (struct sockaddr*) &serverAdresse, sizeof(serverAdresse));
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(atoi(argv[2]));
-	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-
-	connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-
-	if (strcmp(argv[3], "stdin") != 0)
+	/**if(send(clientSocket, saisieUtilisateur, strlen(saisieUtilisateur), 0) < 0)
 	{
-		fd = open(argv[3], O_RDONLY);
-		if (fd < 0)
-		{
-			printf("Erreur d'ouverture du fichier %s\n",argv[3]);
-			exit(-1);
-		}
+		perror("send()");
+		exit(-1);
+	}**/
+
+	fd = open("monTerminal.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Erreur d'ouverture du fichier %s\n", argv[3]);
+		exit(-1);
 	}
 	else fd = STDIN_FILENO;
+	str_cli(fd, clientSocket);
 
-	str_cli(fd, sockfd);
 	exit(0);
 }
 
-void str_cli(int fd, int sockfd)
+void str_cli(int fd, int clientSocket)
 {
-int maxfdp1, stdineof = 0; //stdineof passe à 1 si on a exécuté shutdown sur la socket
-fd_set rset;
-char sendline[MAXLINE], recvline[MAXLINE];
-int n;
+	int maxfdp1, stdineof = 0;
+	fd_set rset;
+	char sendline[MAXCHAR], recvline[MAXCHAR];
+	int n;
 
-if (fd == STDIN_FILENO) printf("\nentrez votre chaine :\n");
-
-FD_ZERO(&rset);
-for ( ; ; )
+	FD_ZERO(&rset);
+	for ( ; ; )
 	{
-	//Si on n'a pas appelé shutdown sur la socket
-	//C'est-à-dire qu'on a encore des données à lire sur l'entrée de données
-	if (stdineof == 0)
+		if (stdineof == 0)
 		{
-		FD_SET(fd, &rset);  //On ajoute l'entrée de données à rset
-		maxfdp1 = max(fd, sockfd) + 1; //on calcule la valeur du plus grand descripteur à laquelle on ajoute 1.
+			FD_SET(fd, &rset); 
+			maxfdp1 = max(fd, clientSocket) + 1;
 		}
-	else maxfdp1 = sockfd+1; //Sinon, il n'y a plus rien à lire sur l'entrée de données, seule la socket sera surveillée
+		else maxfdp1 = clientSocket + 1;
 
-	FD_SET(sockfd, &rset); //On ajoute le descripteur de socket à rset
+		FD_SET(clientSocket, &rset);
+		select(maxfdp1, &rset, NULL, NULL, NULL);
 
-	//On surveille le descripteur de la socket et éventuellement celui de l'entrée de données
-	select(maxfdp1, &rset, NULL, NULL, NULL);
-
-	if (FD_ISSET(sockfd, &rset))
-	{	/* socket is readable */
-		n = read(sockfd, recvline, MAXLINE);
-		if (n == 0) // Cas où read retourne une valeur nulle
-		{
-			// Si on a appelé shutdown sur la socket (stdineof==1)
-			// alors tout est normal, c'est que le serveur a terminé son envoi de données
-			if (stdineof == 1) return; // On sort de la fonction
-			else // Sinon, si on n'a pas appelé shutdown
-			{ // C'est que le serveur est tombé
-				printf("str_cli: serveur termine prematurement\n");
+		if (FD_ISSET(clientSocket, &rset))
+		{	
+			n = read(clientSocket, recvline, MAXCHAR);
+			if (n == 0) 
+			{
+				if (stdineof == 1)
+					return;
+				else
+				{
+					printf("str_cli: serveur termine prematurement\n");
+					exit(-1);
+				}
+			}
+			else if (n < 0)
+			{
+				printf("Erreur de fichier (file)\n");
 				exit(-1);
 			}
-		}
-		else if (n < 0)
-		{
-			printf("Erreur de socket\n");
-			exit(-1);
-		}
-		recvline[n] = '\0';
-		write(STDOUT_FILENO, recvline, strlen(recvline));
-	}
-
-	if (FD_ISSET(fd, &rset)) // Evènement sur l'entrée de données
-		{
-		// Si la lecture sur l'entrée de données renvoie 0
-		// (fin de fichier ou fin de saisie)
-		if ((n = read(fd, sendline,MAXLINE)) == 0)
-		{
-			// On ferme la socket à moitié, seulement en écriture
-			// Pour continuer à recevoir les réponses du serveur
-			// Tout en signalant au serveur que le client a terminé
-			shutdown(sockfd, SHUT_WR);
-			stdineof = 1; // On marque le fait que shutdown a été appelé
-			// Il n'y a plus rien à lire sur l'entrée de données
-			// On ne surveille plus son descripteur
-			// On le retire de rset.
-			FD_CLR(fd, &rset);
-			continue;
+			
+			recvline[n] = '\0';
+			
+			if(strcmp(recvline, "Disconnect") == 0){
+				printf("Vous allez etre deconnecte. Aurevoir.");
+				shutdown(clientSocket, SHUT_WR);
+				continue;
+			}
+			if(strcmp(recvline, "Retry") == 0){
+				printf("Veuillez reessayer svp.");
+				
+			}
+			write(fd, recvline, n);
 		}
 
-		write(sockfd, sendline, n);
+		if (FD_ISSET(fd, &rset))
+		{
+			n = read(fd, sendline, MAXCHAR);
+			if (n == 0)
+			{
+				shutdown(clientSocket, SHUT_WR);
+				FD_CLR(fd, &rset);
+				continue;
+			}else if (n < 0)
+			{
+				printf("Erreur de fichier (socket)\n");
+				exit(-1);
+			}
+
+			sendline[n] = '\0';
+			//printf("je passe ici\n");
+			//printf("=[%s]\n", sendline);
+			write(clientSocket, sendline, n);
+			//sleep(3);
 		}
 	}
 }
--- server.c
+++ server.c
@@ -11,71 +11,175 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-int totaldonnees = 0;
+#define MAXCHAR	256
+
+int totalDonnees = 0, n = 0, retry = 0;
+int/*bool*/ bonjour = 0, bUsername = 0, bPassword = 0; 
+char username[MAXCHAR], password[MAXCHAR];
 
 void str_echo(int);
 
 int main(int argc, char **argv)
 {
-    int lfd,cfd;
-    pid_t childpid;
-    socklen_t clilen;
-    struct sockaddr_in cliaddr,servaddr;
+    int serverSocket, codefd;
+    pid_t childPID;
+    socklen_t lenClientAdresse;
+    struct sockaddr_in clientAdresse, serverAdresse;
     struct sigaction act;
 
     if (argc != 2)
     {
-        printf("Usage : serveur Port\n");
+        printf("<serveur Port>\n");
         exit(-1);
     }
 
-    act.sa_handler=SIG_DFL;
-    act.sa_flags=SA_NOCLDWAIT;
+    act.sa_handler = SIG_DFL;
+    act.sa_flags = SA_NOCLDWAIT;
     sigaction(SIGCHLD, &act, NULL);
 
-    lfd = socket(AF_INET, SOCK_STREAM, 0);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    bzero(&servaddr,sizeof(servaddr));
+    bzero(&serverAdresse, sizeof(serverAdresse));
 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-    servaddr.sin_port=htons(atoi(argv[1]));
+    serverAdresse.sin_family = AF_INET;
+    serverAdresse.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAdresse.sin_port = htons(atoi(argv[1]));
 
-    bind(lfd,(struct sockaddr *) &servaddr, sizeof(servaddr));	
+    bind(serverSocket, (struct sockaddr*) &serverAdresse, sizeof(serverAdresse));	
     perror("bind");
 
-    listen(lfd,10);
+    listen(serverSocket, 10);
 
-    for(;;)
+    for( ; ; )
     {
-        clilen=sizeof(cliaddr);
-        if((cfd=accept(lfd,(struct sockaddr *) &cliaddr,&clilen))<0)
+        
+        lenClientAdresse = sizeof(clientAdresse);
+
+        codefd = accept(serverSocket, (struct sockaddr*) &clientAdresse, &lenClientAdresse);
+        if(codefd < 0)
         {
             printf("erreur accept\n");
             exit(-1);
         }
-        if((childpid=fork())==0)
+
+        childPID = fork();
+        if(childPID == 0)
         {
-            close(lfd);
-            str_echo(cfd);
+            close(serverSocket);
+            str_echo(codefd);
             exit(0);
         }
-        close(cfd);
+        close(codefd);
     }
+    
+    /**if((n = recv(serverSocket, saisieUtilisateur, sizeof saisieUtilisateur - 1, 0)) < 0)
+    {
+        perror("recv()");
+        exit(-1);
+    }
+
+    saisieUtilisateur[n] = '\0';
+    printf("%s\n", saisieUtilisateur);**/
     return 0;
 }
 
 void  str_echo(int sockfd)
 {
 	ssize_t n;
-	char buf[100],final[150];
+	char buf[MAXCHAR];
 
-	while((n=read(sockfd,buf,100))>0)
+	while((n = read(sockfd, buf, MAXCHAR)) > 0)
 	{
-		totaldonnees+=n;
-		buf[n]='\0';
-		//printf("envoi dans socket de %s\n",buf);
-		write(sockfd,buf,n);
+		totalDonnees += n;
+
+        buf[n - 1] = '\0';
+
+
+        if(strcmp(buf, "Bonjour") == 0)
+        {
+            //printf("Who ? %s\n ", buf);
+            strcpy(buf, "Qui etes vous ?\n");
+            //buf[] = "WHO";
+            //buf[n - 1] = '\0';
+            
+            //printf("Who 2 ? %s\n ", buf);
+            bonjour = 1;
+            write(sockfd, buf, strlen(buf));
+            continue;
+        }
+        
+        if(bonjour == 1){
+            if(strlen(username) == 0) {
+                strcpy(username, buf);
+                bUsername = checkUsername(username);
+                if(bUsername == 0) 
+                    strcpy(buf, "Disconnect");
+                else
+                    strcpy(buf, "Ton mot de passe ?");
+
+            }
+            else {
+                strcpy(password, buf);
+                printf("password : %s\n", buf);
+                bPassword = checkUsername(password);
+                if(bPassword == 0) {
+                    if(retry == 3) strcpy(buf, "Disconnect"); 
+                    strcpy(buf, "Retry\n");
+                    retry = retry + 1;
+                }
+                /*else
+                    strcpy(buf, "PasswordFound");*/
+
+            }
+
+            if(bUsername == 1 && bPassword == 2){
+                bonjour = 0;
+                strcpy(buf, "Welcome !\n");
+            }
+        }
+        write(sockfd, buf, strlen(buf));
+		
 	}
-	printf("total données reçues: %d\n",totaldonnees);
+	printf("Total données reçues: %d\n", totalDonnees);
+}
+
+int checkUsername(char str[])
+{
+    FILE* fichier = NULL;
+    char name[MAXCHAR], pwd[MAXCHAR], buff[MAXCHAR];
+ 
+    fichier = fopen("informations.csv", "r");
+ 
+    if (fichier != NULL)
+    {int n;
+        while ((n = fscanf(fichier, "%s %s\n", name, pwd)) != 0)
+        {
+            printf("%d", n);
+            
+            printf("username : %s %s\n", name, pwd);
+
+            if(strcmp(name, str) == 0) return 1;
+            if(strcmp(pwd, str) == 0) return 2;
+        }
+        fclose(fichier);
+    }
+    return 0;
+}
+
+int checkPassword(char str[])
+{
+    FILE* fichier = NULL;
+    char name[MAXCHAR], pwd[MAXCHAR];
+ 
+    fichier = fopen("informations.csv", "r");
+ 
+    if (fichier != NULL)
+    {
+        while (fgets(name, 81, fichier) != NULL)
+        {
+            fscanf(fichier, "%s;%s", name, pwd);
+        }
+        fclose(fichier);
+    }
+    return 0;
 }
